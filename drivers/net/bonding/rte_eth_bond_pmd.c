7a6c9cbbf23a802f531cdca2389d46fcbdf27c1c
ram-yadav <ryadav@juniper.net>
2017-07-19 16:45:43 -0700

    Apply the following patch from Intel to improve LACP performance.
    [PATCH] driver/bond:lacp performance fix
    
    When running in a bonded mode (LACP) with two ports in the bond. When
    one of the ports link is down the performance increases. If the port
    is returned to the bond in link up mode the performance decreases in
    packet rate.
    
    The problem appears to be when polling the slave ports the rx routine
    would always start with the first slave port, which could then starve
    the others ports as the number of packets requested is mostly filled
    by the first port.
    
    The fix here round robins the slave ports on each entry into the bond
    driver rx routine called from the application. Giving the other ports
    a chance to be serviced before packets are dropped.
    
    The patch was tested with RX descriptors set to 256 and not 128 as was
    the default value.
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>
    
    Closes-bug: #1703667

--- drivers/net/bonding/rte_eth_bond_pmd.c
+++ drivers/net/bonding/rte_eth_bond_pmd.c
@@ -145,7 +145,7 @@ bond_ethdev_rx_burst_8023ad(void *queue, struct rte_mbuf **bufs,
 	const uint16_t ether_type_slow_be = rte_be_to_cpu_16(ETHER_TYPE_SLOW);
 	uint16_t num_rx_total = 0;	/* Total number of received packets */
 	uint8_t slaves[RTE_MAX_ETHPORTS];
-	uint8_t slave_count;
+	uint8_t slave_count, idx;
 
 	uint8_t collecting;  /* current slave collecting status */
 	const uint8_t promisc = internals->promiscuous_en;
@@ -159,12 +159,17 @@ bond_ethdev_rx_burst_8023ad(void *queue, struct rte_mbuf **bufs,
 	memcpy(slaves, internals->active_slaves,
 			sizeof(internals->active_slaves[0]) * slave_count);
 
+        idx = internals->active_slave;
+        if (idx >= slave_count) {
+                internals->active_slave = 0;
+                idx = 0;
+        }
 	for (i = 0; i < slave_count && num_rx_total < nb_pkts; i++) {
 		j = num_rx_total;
-		collecting = ACTOR_STATE(&mode_8023ad_ports[slaves[i]], COLLECTING);
+		collecting = ACTOR_STATE(&mode_8023ad_ports[slaves[idx]], COLLECTING);
 
 		/* Read packets from this slave */
-		num_rx_total += rte_eth_rx_burst(slaves[i], bd_rx_q->queue_id,
+		num_rx_total += rte_eth_rx_burst(slaves[idx], bd_rx_q->queue_id,
 				&bufs[num_rx_total], nb_pkts - num_rx_total);
 
 		for (k = j; k < 2 && k < num_rx_total; k++)
@@ -187,7 +192,7 @@ bond_ethdev_rx_burst_8023ad(void *queue, struct rte_mbuf **bufs,
 					!is_same_ether_addr(&bond_mac, &hdr->d_addr)))) {
 
 				if (hdr->ether_type == ether_type_slow_be) {
-					bond_mode_8023ad_handle_slow_pkt(internals, slaves[i],
+					bond_mode_8023ad_handle_slow_pkt(internals, slaves[idx],
 						bufs[j]);
 				} else
 					rte_pktmbuf_free(bufs[j]);
@@ -201,8 +206,10 @@ bond_ethdev_rx_burst_8023ad(void *queue, struct rte_mbuf **bufs,
 			} else
 				j++;
 		}
+                if (unlikely(++idx == slave_count))
+                        idx = 0;
 	}
-
+        internals->active_slave = idx;
 	return num_rx_total;
 }
 
