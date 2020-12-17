80da96ac2acbee1fbaf1f10d66993c66ebcec02a
Nan Wang <nanw@juniper.net>
2020-12-17 23:21:10 +0530

    Closes-Jira-Bug: CEM-20520
    DPDK library does not respond to LACP partner's change in System ID,
    change is added so that DPDK can dynamically add or delete its LAG member.
    
    Change-Id: Iaacfbae3f4b1700bea9084d5372f63e6c286028c
    Signed-off-by: Nan Wang <nanw@juniper.net>

--- drivers/net/bonding/rte_eth_bond_8023ad.c
+++ drivers/net/bonding/rte_eth_bond_8023ad.c
@@ -684,18 +684,20 @@ selection_logic(struct bond_dev_private *internals, uint8_t slave_id)
 	slaves_count = internals->active_slave_count;
 	port = &mode_8023ad_ports[slave_id];
 
-        /* if port is selected, do nothing 
-         * Rx machine may set the variable to UNSELECTED when it detects a 
-         * change in protocol (ie Key) that materially affects the link's 
-         * membership in its current LAG */
-        if (port->selected == SELECTED ) {
-            return;
-        }    
+	/*
+	 * if port is selected, do nothing
+	 * Rx machine may set the variable to UNSELECTED when it detects a
+	 * change in protocol (ie Key) that materially affects the link's
+	 * membership in its current LAG
+	 */
+	if (port->selected == SELECTED ) {
+		return;
+	}
 
 	MODE4_DEBUG(" slave id=%3u aggregator id=%3u slaves count=%3u\n",
-                        slave_id,
-			port->aggregator_port_id,
-			slaves_count);
+		slave_id,
+		port->aggregator_port_id,
+		slaves_count);
 
 	/* Search for aggregator suitable for this port */
 	for (i = 0; i < slaves_count; ++i) {
@@ -733,14 +735,14 @@ selection_logic(struct bond_dev_private *internals, uint8_t slave_id)
 		new_agg_id = mode_band_id;
 		break;
 	case AGG_STABLE:
-                /* FIXME if condition can never be true */
+		/* FIXME if condition can never be true */
 		if (default_slave == slaves_count)
 			new_agg_id = slave_id;
 		else
 			new_agg_id = slaves[default_slave];
 		break;
 	default:
-                /* FIXME if condition can never be true */
+		/* FIXME if condition can never be true */
 		if (default_slave == slaves_count)
 			new_agg_id = slave_id;
 		else
@@ -758,18 +760,18 @@ selection_logic(struct bond_dev_private *internals, uint8_t slave_id)
 				"aggregator not found, using default" : "aggregator found",
 			port->aggregator_port_id);
 
-                 if (port->aggregator_port_id != slave_id) {
-                     port->selected = SELECTED;
-                 }
+		if (port->aggregator_port_id != slave_id) {
+			port->selected = SELECTED;
+		}
 	} else {
-            if (conform == slaves_count) {
-                port->selected = SELECTED;
-            }    
-        }
+		if (conform == slaves_count) {
+			port->selected = SELECTED;
+		}
+	}
 
-        if (SM_FLAG(port, BEGIN)) {
-  	   port->selected = SELECTED;
-        }   
+	if (SM_FLAG(port, BEGIN)) {
+		port->selected = SELECTED;
+	}
 }
 
 /* Function maps DPDK speed to bonding speed stored in key field */
